9599ad7c8e7a2551da133d7e0b46de2554b12483
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-25 11:20:57 -0500

    feat: base express app set up

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+require('dotenv/config');
 
+const path = require('node:path');
+const express = require('express');
+
+const assetsPath = path.join(__dirname, 'views');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.set('views', assetsPath);
+app.set('view engine', 'ejs');
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
