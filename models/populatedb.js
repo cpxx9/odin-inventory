da94e10e7b2c86f651107aeb50d2b349aae89dd4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-25 08:23:35 -0500

    fix: db script working

--- models/populatedb.js
+++ models/populatedb.js
@@ -19,15 +19,10 @@ CREATE TABLE IF NOT EXISTS studio(
   CONSTRAINT studio_pkey PRIMARY KEY (studio_id)
 );
 
-CREATE TABLE IF NOT EXISTS game(
+CREATE TABLE game(
   game_id INTEGER GENERATED ALWAYS AS IDENTITY,
   title VARCHAR ( 255 ),
-  genre_id INTEGER NOT NULL,
-  CONSTRAINT game_pkey PRIMARY KEY (game_id),
-  CONSTRAINT game_genre_id_fkey FOREIGN KEY (genre_id)
-    REFERENCES genre (genre_id) MATCH SIMPLE
-      ON UPDATE CASCADE
-      ON DELETE RESTRICT
+  CONSTRAINT game_pkey PRIMARY KEY (game_id)
 );
 
 CREATE TABLE IF NOT EXISTS game_studio(
@@ -40,6 +35,20 @@ CREATE TABLE IF NOT EXISTS game_studio(
       ON DELETE RESTRICT,
   CONSTRAINT game_studio_studio_id_fkey FOREIGN KEY (studio_id)
     REFERENCES studio (studio_id) MATCH SIMPLE
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS game_genre(
+  game_id INTEGER NOT NULL,
+  genre_id INTEGER NOT NULL,
+  CONSTRAINT game_genre_pkey PRIMARY KEY (game_id, genre_id),
+  CONSTRAINT game_genre_game_id_fkey FOREIGN KEY (game_id)
+    REFERENCES game (game_id) MATCH SIMPLE
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
+  CONSTRAINT game_genre_genre_id_fkey FOREIGN KEY (genre_id)
+    REFERENCES genre (genre_id) MATCH SIMPLE
       ON UPDATE CASCADE
       ON DELETE RESTRICT
 );
