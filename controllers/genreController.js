4e04c4fda59ea0bc52e1de2fdbd3a281d4216366
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-26 21:19:58 -0500

    refactor: remove console log

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,6 @@ const { getGenre } = require('../models/queries');
 const displayGenre = async (req, res) => {
   const { genre } = req.params;
   const queryGenre = await getGenre(genre);
-  console.log(queryGenre);
   res.render('genre', { genre, queryGenre });
 };
 
